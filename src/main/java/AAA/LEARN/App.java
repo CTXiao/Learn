5bc43d8979014663289812c18995d669417b6350
CTXiao <294295992@qq.com>
2018-03-03 11:54:36 +0800

    整理一下

--- src/main/java/AAA/LEARN/App.java
+++ src/main/java/AAA/LEARN/App.java
@@ -1,9 +1,13 @@
-package AOP.AOP;
+package AAA.LEARN;
 
 import java.text.DecimalFormat;
 
 import org.springframework.aop.framework.ProxyFactory;
 
+import AOP.AOP.AfterAdvice;
+import AOP.AOP.BeforeAdvice;
+import AOP.AOP.Singer;
+import AOP.AOP.SurroundAdvice;
 import Collection.HashMap.HashMapTest;
 import SOLID.LSP.GolbalSetting;
 import SOLID.LSP.GuestSetting;
@@ -12,22 +16,20 @@ import SOLID.OCP.TaxCalculator;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-//    	...AOP...
-//    	AOP();
-//    	开放封闭原则
-//    	OCP();
-//    	里氏替换原则
-//    	LSP();
-    	new HashMapTest();
-    }
+public class App {
+	public static void main(String[] args) {
+		// ...AOP...
+		// AOP();
+		// 开放封闭原则
+		// OCP();
+		// 里氏替换原则
+		// LSP();
+		new HashMapTest();
+	}
+
 	private static void AOP() {
-        System.out.println( "敬礼！！" );
+		System.out.println("敬礼！！");
 		ProxyFactory factory = new ProxyFactory();
 		factory.setTarget(new Singer());
 		factory.addAdvice(new BeforeAdvice());
@@ -36,21 +38,20 @@ public class App
 		Singer singer = (Singer) factory.getProxy();
 		singer.sing("人民公安向前进");
 	}
+
 	private static void OCP() {
-        TaxCalculator tc = new TaxCalculator();
-        DecimalFormat df = new DecimalFormat(".0");
-        String string = tc.calculate(df,new CountryCalculatorForUK());
-        System.out.println(string);
+		TaxCalculator tc = new TaxCalculator();
+		DecimalFormat df = new DecimalFormat(".0");
+		String string = tc.calculate(df, new CountryCalculatorForUK());
+		System.out.println(string);
 	}
-	
+
 	/**
-	 * LSP()
-	 * 分离Set和Get的想法来自于Guset没有Set的权限。
-	 * 若不把Set和Get分离，而写在同一个ISetting接口中，
+	 * LSP() 分离Set和Get的想法来自于Guset没有Set的权限。 若不把Set和Get分离，而写在同一个ISetting接口中，
 	 * 那么当guset调用set方法时，会出现异常。
 	 */
 	private static void LSP() {
-		
+
 		GolbalSetting gg = new GolbalSetting();
 		gg.SetSetting();
 		gg.GetSetting();
